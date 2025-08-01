fce4503333068150db47b8e437a19ea5f2dfa8bb
mathn00b <maxim.cherbadzhi@shelterzoom.com>
2025-08-01 12:49:15 +1000

    Fix images

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/beleneva-cinema-landing',
-  assetPrefix: '/beleneva-cinema-landing/',
   eslint: {
     ignoreDuringBuilds: true,
   },
